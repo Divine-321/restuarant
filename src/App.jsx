67df873e092e6d95d719baf93c42196de8d52e83
Favour <divinexcell321@gmail.com>
2026-02-16 23:58:13 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import HeroSection from "./components/HeroSection";
 
 const App = () => {
   return (
-  
+  <div className="w-screen h-screen relative">
     <HeroSection />
+  </div>
   );
 };
 
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,22 @@
-import { VIDEO_URL } from "./utils/app.constants";
+import { APP_STORE_URL, PLAY_STORE_URL, VIDEO_URL } from "./utils/app.constants";
 
 const HeroSection = () => {
     return (
         <div className="w-screen h-screen relative">
-            <video src={VIDEO_URL} autoPlay loop playsInline/>
+            <video src={VIDEO_URL} autoPlay loop playsInline muted/>
+
+            <div className="z-10 absolute inset-0 bg-linear-to-t from-black/80 via-black/20 to-transparent">
+            <div className="flex flex-col items-center justify-center h-full pt-80">
+                <img src="/assets/zomato.png" alt="zomato-icon" className="w-52" />
+                <div className="mb-5 px-5 text-center font-semibold text-5xl text-white mt-8">India's #1 <br /> food delivery app</div>
+                <span className="text-white text-2xl px-5 text-center font-medium">Experience fast & easy online ordering <br /> on the zomato app</span>
+
+                <div>
+                    <img src={PLAY_STORE_URL} className="w-44" />
+                    <img src={APP_STORE_URL} className="w-44" />
+                </div>
+            </div>
+            </div>
         </div>
     );
 };
